cd6b3e96042aab7dbe004ebad078884acf0c2755
QiMing Wang <33197116+Wonkmy@users.noreply.github.com>
2020-06-30 12:48:44 +0800

    Add files via upload
    自制的相机跟随脚本

---
+++ CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public static CameraFollow instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+	public Transform target;
+	public Vector3 offset;
+	private Vector3 cur_velo;
+	void FixedUpdate () {
+		transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref cur_velo, 0.5f);
+	}
+}
